1e384c177e3c5dc71322dbb95a1879a0a02f9cea
amirh56 <amirhossein.hamidian@ronash.co>
2019-07-19 21:57:59 +0430

    fix full screen activity

--- app/src/main/java/com/findthespy/app/CategoriesListActivity.java
+++ app/src/main/java/com/findthespy/app/CategoriesListActivity.java
@@ -1,12 +1,12 @@
 package com.findthespy.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,19 +14,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class CategoriesListActivity extends AppCompatActivity implements OnItemClickListener {
+public class CategoriesListActivity extends FullscreenActivity implements OnItemClickListener {
 
     private RecyclerView recyclerView;
     private CategoryRecyclerViewAdapter mAdapter;
     private LinearLayoutManager linearLayoutManager;
     private GridLayoutManager gridLayoutManager;
     private ArrayList<String> categories = new ArrayList<>();
+    private InputMethodManager imm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        super.mContentView = findViewById(R.id.fullscreen_content);
+
         final EditText nameEditText = findViewById(R.id.category_name);
+        imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
 
 
         final TinyDB db = new TinyDB(getApplicationContext());
@@ -48,10 +53,17 @@ public class CategoriesListActivity extends AppCompatActivity implements OnItemC
                 categories.add(nameEditText.getText().toString());
                 db.putListString("categories", categories);
                 nameEditText.setText("");
+                nameEditText.clearFocus();
+                closeKeyBoard();
                 mAdapter.notifyDataSetChanged();
             }
         });
 
+    }
+
+    private void closeKeyBoard() {
+        if (imm != null)
+            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
     }
 
     @Override
